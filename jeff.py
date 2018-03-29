79ca85029f0225ef008a8b69b9fc52e3525fa7e2
Jeff <limziwangis@gmail.com>
2018-03-29 18:38:01 +0800

    did some work

--- jeff.py
+++ jeff.py
@@ -1,16 +1,5 @@
-# this is pseudo code.
-#starts with
 
-list_dates = [date1, date2, date3]
+# JSON Convert data to CSV
+import json
 
-list_diff = []
-
-for i in range(len(list_dates)):
-    diff = list_dates[i] - list_dates[i-1]
-    list_diff.append(diff)
-
-average(list_diff)
-
-#something along these lines
-
-
+json_parsed = json.loads(json_data)
